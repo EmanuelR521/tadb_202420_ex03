f603a81b029875241489404ba8285cba974df105
Emanuel Rios <118300687+EmanuelR521@users.noreply.github.com>
2024-11-09 02:01:10 -0500

    feat: add perfil de ejecucion y componentes a utilizar en el program

--- COMPUESTOS_QUIMICOS_CS_REST_SQL_API/COMPUESTOS_QUIMICOS_CS_REST_SQL_API/Program.cs
+++ COMPUESTOS_QUIMICOS_CS_REST_SQL_API/COMPUESTOS_QUIMICOS_CS_REST_SQL_API/Program.cs
@@ -1,6 +1,25 @@
+using COMPUESTOS_QUIMICOS_CS_REST_SQL_API.DbContexts;
+using COMPUESTOS_QUIMICOS_CS_REST_SQL_API.Interfaces;
+using COMPUESTOS_QUIMICOS_CS_REST_SQL_API.Repositories;
+using COMPUESTOS_QUIMICOS_CS_REST_SQL_API.Services;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//DBContext
+builder.Services.AddSingleton<PgsqlDbContext>();
+
+//Los repositorios
+builder.Services.AddScoped<IResumenRepository, ResumenRepository>();
+builder.Services.AddScoped<IElementoRepository, ElementoRepository>();
+builder.Services.AddScoped<ICompuestoRepository, CompuestoRepository>();
+
+//servicios asociados para cada ruta
+builder.Services.AddScoped<ResumenService>();
+builder.Services.AddScoped<ElementoService>();
+builder.Services.AddScoped<CompuestoService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
