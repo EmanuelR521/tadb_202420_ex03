ba871ac5be2dae27f8580d4dbe4bc5883fcb1ad8
Carlos Sanabria <carlos.sanabria@upb.edu.co>
2024-11-09 12:30:26 -0500

    feat: implementacion inicial Models

---
+++ COMPUESTOS_QUIMICOS_CS_REST_SQL_API/COMPUESTOS_QUIMICOS_CS_REST_SQL_API/Models/Compuesto.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace COMPUESTOS_QUIMICOS_CS_REST_SQL_API.Models
+{
+    public class Compuesto
+    {
+        [JsonPropertyName("uuid")]
+        public Guid Uuid { get; set; } = Guid.Empty;
+
+        [JsonPropertyName("nombre")]
+        public string? Nombre { get; set; } = string.Empty;
+
+        [JsonPropertyName("simbolo")]
+        public string? Simbolo { get; set; } = string.Empty;
+
+        [JsonPropertyName("numero_atomico")]
+        public int Numero_Atomico { get; set; } = 0;
+
+        [JsonPropertyName("config_electronica")]
+        public string? Config_Electronica { get; set; } = string.Empty;
+
+    }
+}
---
+++ COMPUESTOS_QUIMICOS_CS_REST_SQL_API/COMPUESTOS_QUIMICOS_CS_REST_SQL_API/Models/Elemento_Por_Compuesto.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace COMPUESTOS_QUIMICOS_CS_REST_SQL_API.Models
+{
+    public class Elemento_Por_Compuesto
+    {
+        [JsonPropertyName("elemento_uuid")]
+        public Guid Elemento_Uuid { get; set; }
+
+        [JsonPropertyName("compuesto_uuid")]
+        public Guid Compuesto_Uuid { get; set; }
+
+        [JsonPropertyName("cantidad_atomos")]
+        public int Cantidad_Atomos { get; set; }
+    }
+}
