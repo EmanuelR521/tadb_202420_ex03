6bec2aa3c60cf8785ea1f3a8fee87d7ed4f13c00
Emanuel Rios <118300687+EmanuelR521@users.noreply.github.com>
2024-11-09 01:47:01 -0500

    feat: add creacion del proyecto

---
+++ COMPUESTOS_QUIMICOS_CS_REST_SQL_API/COMPUESTOS_QUIMICOS_CS_REST_SQL_API/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace COMPUESTOS_QUIMICOS_CS_REST_SQL_API
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
