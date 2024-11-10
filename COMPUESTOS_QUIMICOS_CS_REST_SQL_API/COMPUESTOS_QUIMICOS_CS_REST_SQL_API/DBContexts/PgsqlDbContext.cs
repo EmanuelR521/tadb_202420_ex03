8c53340fd85f6e1b39456cf0fdc6a68015ed7610
Carlos Sanabria <carlos.sanabria@upb.edu.co>
2024-11-10 00:27:28 -0500

    feat: implementacion DBContexts and appsettings.json

---
+++ COMPUESTOS_QUIMICOS_CS_REST_SQL_API/COMPUESTOS_QUIMICOS_CS_REST_SQL_API/DBContexts/PgsqlDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace COMPUESTOS_QUIMICOS_CS_REST_SQL_API.DBContexts
+{
+    public class PgsqlDbContext
+    {
+        private readonly string cadenaConexion;
+
+        public PgsqlDbContext(IConfiguration unaConfiguracion)
+        {
+            cadenaConexion = unaConfiguracion.GetConnectionString("CompuestoQuimico")!;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new NpgsqlConnection(cadenaConexion);
+        }
+    }
+}
